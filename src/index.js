9bad0bad380c1a9005910b8bca08a1c57381d690
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-14 21:41:07 +0500

    Update routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App";
+import FreeCourses from "./pages/FreeCourses";
+import PaidCourses from "./pages/PaidCourses";
+import Teachers from "./pages/Teachers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,6 +13,9 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/free-courses" element={<FreeCourses />} />
+        <Route path="/paid-courses" element={<PaidCourses />} />{" "}
+        <Route path="/teachers" element={<Teachers />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
