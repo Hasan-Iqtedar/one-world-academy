fabb87dfaa73ed138d1d3298c64d322463cfaea8
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-15 19:34:39 +0500

    Add click listener

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import styles from "../styles/card.module.css";
 
 const Card = (props) => {
   return (
-    <div className={styles.card} style={props.style}>
+    <div className={styles.card} style={props.style} onClick={props.onClick}>
       {props.children}
     </div>
   );
